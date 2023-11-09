189adaf35ad63fb537f2c2894ef0fd5499283407
arunlajayan <arunlajayan@gmail.com>
2023-11-09 11:08:13 +0530

    function directly import with module.export

--- https.js
+++ https.js
@@ -1,8 +1,8 @@
-const request = require('./request');
-const response = require('./response');
+const {send} = require('./request');
+const {read} = require('./response');
 function make(url, data) {
-  request.send(url, data);
-  return response.read();
+  send(url, data);
+  return read();
 }
 const responseData = make('https://google.com');
 console.log(responseData);
